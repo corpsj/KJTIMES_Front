0c64c7eac05a0e65bbc99cf0dde0aab75e2dc6bf
zoopark <zoopark@zooparks-iMac.local>
2026-01-29 06:40:35 +0900

    Update footer details

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -11,9 +11,9 @@ export function Footer() {
                         <Text fw={700} mb="xs">광전타임즈</Text>
                         <Text size="sm" c="dimmed" style={{ lineHeight: 1.6 }}>
                             전남 함평군 함평읍 영수길 148 2층<br />
-                            등록번호: [전남, 아00000] | 발행·편집인: [이름]<br />
-                            전화: 061-000-0000 | 팩스: 061-000-0000 | 이메일: email@kjtimes.co.kr<br />
-                            사업자등록번호: 000-00-00000
+                            등록번호: [준비중] | 발행·편집인: 선종인<br />
+                            전화: 010-1234-5678 | 이메일: test@kjtimes.co.kr<br />
+                            사업자등록번호: [준비중]
                         </Text>
                         <Text size="sm" c="dimmed" mt="xs">
                             Copyright © Kwangjeon Times. All rights reserved.
