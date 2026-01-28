d4464f30ef6f0c9cd8befc1e68b04002206ec03d
zoopark <zoopark@zooparks-iMac.local>
2026-01-29 03:35:24 +0900

    Update footer with company address

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -9,9 +9,11 @@ export function Footer() {
                 <Group justify="space-between" align="start">
                     <Box>
                         <Text fw={700} mb="xs">광전타임즈</Text>
-                        <Text size="sm" c="dimmed">
-                            광주광역시 ... <br />
-                            등록번호: ... | 발행인: ...
+                        <Text size="sm" c="dimmed" style={{ lineHeight: 1.6 }}>
+                            전남 함평군 함평읍 영수길 148 2층<br />
+                            등록번호: [전남, 아00000] | 발행·편집인: [이름]<br />
+                            전화: 061-000-0000 | 팩스: 061-000-0000 | 이메일: email@kjtimes.co.kr<br />
+                            사업자등록번호: 000-00-00000
                         </Text>
                         <Text size="sm" c="dimmed" mt="xs">
                             Copyright © Kwangjeon Times. All rights reserved.
