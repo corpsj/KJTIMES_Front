f511d9766324fc0fdaf763b00e640e7232b4aa7d
corpsj <corpsj@users.noreply.github.com>
2026-02-11 02:12:52 +0900

    M-02: Redirect /login to /admin/login
    - Replaced non-functional login page with server redirect
    - Eliminates dead page with copied Mantine placeholder

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,26 +1,5 @@
-import { Container, Title, TextInput, PasswordInput, Button, Paper, Group, Anchor, Stack } from "@mantine/core";
+import { redirect } from "next/navigation";
 
-export default function Login() {
-    return (
-        <Container size={420} my={40}>
-            <Title ta="center" order={1}>
-                로그인
-            </Title>
-
-            <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-                <Stack gap="md">
-                    <TextInput label="이메일" placeholder="you@mantine.dev" required />
-                    <PasswordInput label="비밀번호" placeholder="Your password" required />
-                    <Group justify="space-between" mt="lg">
-                        <Anchor component="button" size="sm">
-                            비밀번호 찾기
-                        </Anchor>
-                    </Group>
-                    <Button fullWidth mt="xl">
-                        로그인
-                    </Button>
-                </Stack>
-            </Paper>
-        </Container>
-    );
+export default function LoginRedirect() {
+    redirect("/admin/login");
 }
